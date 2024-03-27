f3396e6ddbba70eb430c771033ba7f6f5c721059
HylaDev <ernestdossa.9@gmail.com>
2024-03-27 10:58:41 +0100

    update projects component

--- src/Components/Portofolio/Projects.js
+++ src/Components/Portofolio/Projects.js
@@ -3,6 +3,7 @@ import underlineB from '../../Images/Double traits_blanc.svg';
 import './Project.css';
 import ayikan from '../../Images/projects/ayikan.png';
 import yieldy from '../../Images/projects/yieldy.png';
+import portofolio from '../../Images/projects/portofolio.png';
 
 
 class Projects extends Component {
@@ -10,10 +11,10 @@ class Projects extends Component {
         return (
             <div id='projects' className='container-fluid'>
                 <div className="d-flex justify-content-center align-items-center mb-4">
-                    <h4>Projects<br /><img className='text-center' src={underlineB} alt="uderline"/></h4>
+                    <h4>Projects I've worked on<br /><img className='text-center' src={underlineB} alt="uderline"/></h4>
                 </div>
                 <div className='row'>
-                    <div className='col-md-6 my-2 cards p-1'>
+                    <div className='col-md-4 my-2 cards p-1'>
                         <a href='https://www.yieldy.fr/' target='blank' title='Cliquer ici'>
                         <div className="card">
                             <img src={yieldy} className="card-img-top" alt="..." />
@@ -31,7 +32,7 @@ class Projects extends Component {
                             
                         </div>*/}
                     </div>
-                    <div className='col-md-6 my-2 cards p-1 '>
+                    <div className='col-md-4 my-2 cards p-1 '>
                         <a href='http://ayikan.com/' target='blank'>
                             <div className="card">
                                 <img src={ayikan} className="card-img-top" alt="..." />
@@ -39,6 +40,25 @@ class Projects extends Component {
                                     <h5 class="card-title">Ayikan</h5>
                                     <p class="card-text text-justify">
                                         Ayikan is an e-commerce application for online watch purchases made with Django-Oscar.
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                        {/*
+                        <div id="technos">
+                            
+                        </div>*/}
+                    </div>
+
+                    <div className='col-md-4 my-2 cards p-1 '>
+                        <a href='http://yelian.netlify.app/' target='self'>
+                            <div className="card">
+                                <img src={portofolio} className="card-img-top" alt="Portofolio home page" />
+                                <div class="card-body">
+                                    <h5 class="card-title">My website</h5>
+                                    <p class="card-text text-justify">
+                                        My portofolio was created to increase my visibility and my search for a work-study placement. 
+                                        This project shows my skills in responsive web development and the different projects I've worked on. 
                                     </p>
                                 </div>
                             </div>
